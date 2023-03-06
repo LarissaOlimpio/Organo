59c72c7ab176daae91c61ee538f52fe835ef833d
Larissa Olimpio <lrsolimpio@gmail.com>
2023-03-06 17:20:56 -0300

    atualizando o css dos card dos colaboradores

--- src/componentes/Collaborator/index.jsx
+++ src/componentes/Collaborator/index.jsx
@@ -3,12 +3,12 @@ import styles from'./Collaborator.module.css'
 import iconFavorite from './iconFavorite.png'
 import iconNotfavor from './iconNotfavor.png'
 
-
 const Collaborator = ({ name,image,position,backgroundColor,date,id}) => {
 
     const {favorite, addFavorite} = useFavoriteContext();
     const isFavorite = favorite.some((fav) => fav.id === id);
     const icon = !isFavorite ? iconFavorite : iconNotfavor;
+    
 
     return (
         <div className={styles.collaborator}>
@@ -27,6 +27,7 @@ const Collaborator = ({ name,image,position,backgroundColor,date,id}) => {
                 <h4>{name}</h4>
                 <h5>{position}</h5>
                 <h5>{new Date(date).toLocaleDateString()}</h5>
+                  {/*toLocaleDateString analisa o idioma do navegador e forma a data conforme a localização */}
 
                 <img 
                     src={icon}
@@ -35,10 +36,7 @@ const Collaborator = ({ name,image,position,backgroundColor,date,id}) => {
                     onClick={() => {
                         addFavorite({ name,image,position,backgroundColor,date,id})}
                 } />
-               
-                {/*toLocaleDateString analisa o idioma do navegador e forma a data conforme a localização */}
-
-    
+             
             </div>
         </div>
 )}
